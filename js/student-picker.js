907ee42ff0e4180b1842c7a8be8ef6c673cd3e72
Justin Reich <justinreich.dev@gmail.com>
2019-02-27 10:28:14 -0600

    add zion students

--- js/student-picker.js
+++ js/student-picker.js
@@ -1,7 +1,30 @@
 "use strict";
 
-// populate with the current cohort
-const students = [];
+const students = [
+    "Adrien",
+    "Anwar",
+    "Arash",
+    "Bethany",
+    "Blossom",
+    "Carlo",
+    "Cerissa",
+    "Cory",
+    "Dwight",
+    "Erik",
+    "James",
+    "Jeremy",
+    "Josh",
+    "Luis",
+    "Marcos",
+    "Megan",
+    "Mike",
+    "Mindy",
+    "Patrick",
+    "Sabita",
+    "Tim",
+    "Tony",
+    "Travis"
+];
 
 const randomIndex = Math.floor(Math.random() * (students.length - 1));
 document.querySelector('#student-name').innerText = students[randomIndex];
